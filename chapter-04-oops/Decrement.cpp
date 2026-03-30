c752ad70a21e3eb3eecb7754ef208d6222584283
Abhishek Kumar <143705430+Abhishekaaryabgp@users.noreply.github.com>
2026-03-30 14:41:36 +0530

    Delete chapter-04-oops directory

--- chapter-04-oops/Decrement.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int var = - -37677;// Here unary operator uses twicce so the expression is positive type
-    cout << var;
-    system("pause>0");
-    return 0;
-}
